40823b474111af736b1fc67865770a963f3ef3cc
Human Gamer <39096122+HumanGamer@users.noreply.github.com>
2022-08-08 18:02:45 -0500

    Basic Raylib Project

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,44 @@
-#include <iostream>
+#include "raylib.h"
+
+#if defined(PLATFORM_WEB)
+#include <emscripten/emscripten.h>
+#endif
+
+int screenWidth = 800;
+int screenHeight = 450;
+
+void UpdateDrawFrame(void);
 
 int main()
 {
-    std::cout << "Hello, World!" << std::endl;
+    InitWindow(screenWidth, screenHeight, "Test Game");
+
+#if defined(PLATFORM_WEB)
+    emscripten_set_main_loop(UpdateDrawFrame, 0, 1);
+#else
+    SetTargetFPS(60);
+
+    SetExitKey(KEY_NULL);
+
+    // Main game loop
+    while (!WindowShouldClose())    // Detect window close button or ESC key
+    {
+        UpdateDrawFrame();
+    }
+#endif
+
+    CloseWindow();
+
     return 0;
+}
+
+void UpdateDrawFrame(void)
+{
+    BeginDrawing();
+    {
+        ClearBackground(RAYWHITE);
+
+        DrawText("Congrats! You created your first window!", 190, 200, 20, LIGHTGRAY);
+    }
+    EndDrawing();
 }
